0dbc8269e63c5ad3b9db7048d8d17c439ce38bd2
nghiahsgs <nghiahsgs@gmail.com>
2021-07-17 10:23:21 +0700

    make it better

--- usage.py
+++ usage.py
@@ -3,8 +3,11 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-host = os.getenv('host')
-dcom_port = '4001'
+# host = os.getenv('host')
+# dcom_port = '4001'
+
+host = '192.168.0.102'
+dcom_port = '4000'
 
 http_proxy  = f"http://{host}:{dcom_port}"
 https_proxy = f"https://{host}:{dcom_port}"
@@ -12,6 +15,7 @@ proxyDict = {
     "http"  : http_proxy, 
     "https" : https_proxy
 }
-url = 'https://api.myip.com'
+# url = 'https://api.myip.com'
+url = 'http://httpbin.org/ip'
 res = requests.get(url, proxies=proxyDict).json()
 print(res)
